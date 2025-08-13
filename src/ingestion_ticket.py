ec4fbe7badd3d4157de0802e2d56328c8dfe6a43
Louis903995 <louis@moises.fr>
2025-08-13 18:09:39 +0200

    creation de la fonction d'ingestion de ticket

---
+++ src/ingestion_ticket.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+
+# transforme l'image en un json (dict) intermédiaire
+def ocr(image: bytes) -> dict:
+    return {
+        "entete": {
+            "nom_enseigne": "Carrefour Market Loos",
+            "date_heure_ticket": datetime(2025, 8, 10, 10, 00, 00),
+            "montant_total_ticket": 45.8,
+        },
+        "lignes": [
+            {
+                "libelle_produit": "Coca cola",
+                "quantite": 1,
+                "prix_unitaire": 12,
+                "montant_total_ligne": 12.1,
+            },
+            {
+                "libelle_produit": "Perrier 50 cl",
+                "quantite": 2,
+                "prix_unitaire": 2,
+                "montant_total_ligne": 4,
+            },
+            # etc....
+        ],
+    }
+
+
+# retrouve l'id de l'enseigne en fonction du nom d'enseigne scanné
+# et ajoute la propriété "id_enseigne" ainsi que la valeur de l'id
+def resoud_enseigne(ticket: dict) -> dict:
+    return ticket
+
+
+# itere sur toutes les lignes du ticket et
+# ajoute la propriété "categorie_produit_id" lorsque elle est trouvée
+def categorise_produits(ticket: dict) -> dict:
+    return ticket
+
+
+# ajoute le nouvel entete dans la table "ticket_entete"
+# renvoie l'identifiant du ticket, None en cas d'erreur
+def write_ticket_entete(user_id, ticket: dict) -> int | None:
+    return 1
+
+
+# ajoute chacune des lignes de tickets dans la table "ticket_ligne"
+# renvoie le nombre de lignes écrites, None en cas d'erreur
+def write_ticket_ligne(ticket_id: int, ticket: dict) -> int | None:
+    return 2
+
+
+# analyse une image de ticket et le stocke dans les différentes tables en l'attachant au user id
+# renvoie l'id du ticket, None en cas d'erreur
+def ingestion_image(user_id: int, image: bytes) -> int | None:
+    ticket_lu = resoud_enseigne(categorise_produits(ocr(image)))
+    ticket_id = write_ticket_entete(user_id, ticket_lu)
+    if ticket_id:
+        lignes_ecrites = write_ticket_ligne(ticket_id, ticket_lu)
+        if lignes_ecrites == len(ticket_lu["lignes"]):
+            return ticket_id
+    return None
---
+++ tests/ingestion_ticket_test.py
@@ -0,0 +1,22 @@
+from ingestion_ticket import ingestion_image
+import base64
+
+
+def encode_image(image_path: str) -> bytes | None:
+    """Encode the image to base64."""
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+    except FileNotFoundError:
+        print(f"Error: The file {image_path} was not found.")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+image_path = "images/carrefour1.jpeg"
+base64_image = encode_image(image_path)
+
+resultat = ingestion_image(1, base64_image)
+print(resultat)
