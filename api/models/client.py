9ee291ea3b765f50df8ded485636f68db03a482d
Louis903995 <louis@moises.fr>
2025-08-15 16:45:56 +0200

    mise à jour du SQLModel Client

--- api/models/client.py
+++ api/models/client.py
@@ -4,10 +4,10 @@ import datetime
 
 class Client(SQLModel, table=True):
     client_id: Optional[int] = Field(default=None, primary_key=True)
-    nom: str = Field(max_length=100)
-    prenom: str = Field(max_length=100)
-    email: Optional[str] = Field(default=None, max_length=100, unique=True)
-    adresse: Optional[str] = Field(default=None, max_length=100, unique=True)
-    budget: float = Field(default=0.0)
-    date_enregistrement: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
-    date_derniere_modification: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+    nom_client: str = Field(max_length=100)
+    prenom_client: str = Field(max_length=100)
+    email_client: Optional[str] = Field(default=None, max_length=100, unique=True)
+    adresse_client: Optional[str] = Field(default=None, max_length=100, unique=True)
+    budget_client: float = Field(default=0.0)
+    date_creation: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+    date_modification: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
