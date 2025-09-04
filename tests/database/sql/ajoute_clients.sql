d91aa385827380966eb5a2e9df4505b5640af697
Louis903995 <louis@moises.fr>
2025-09-04 15:47:02 +0200

    changé qq data d'exemple pour mieux visualiser les exemples

--- tests/database/sql/ajoute_clients.sql
+++ tests/database/sql/ajoute_clients.sql
@@ -5,4 +5,4 @@ VALUES
     ('Martin', 'Sophie', 'sophie.martin@email.com', '2 rue des Lilas', 300.00, '2025-08-14T09:54:03.753', '2025-08-14T09:54:03.753'),
     ('Lemoine', 'Claire', 'claire.lemoine@email.com', '3 rue des Violettes', 150.00, '2025-08-14T09:54:03.770', '2025-08-14T09:54:03.770'),
     ('Moises', 'Louis', 'louis.moises@email.com', '4 rue des Marguerites', 162.00, '2025-08-14T09:54:03.793', '2025-08-14T09:54:03.793'),
-    ('Brad', 'Pitt', 'brad.pitt@email.com', '5 rue des Tulipes', 89.00, '2025-08-14T09:54:03.810', '2025-08-14T09:54:03.810');
+    ('Brad', 'Pitt', 'brad.pitt@email.com', '5 rue des Tulipes', 30.00, '2025-08-14T09:54:03.810', '2025-08-14T09:54:03.810');
